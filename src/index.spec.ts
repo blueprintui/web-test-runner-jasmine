6ff985a0532b8c445f145987b9a7d36c18a3f4c5
Giancarlo Calderón Cárdenas <gian1200@hotmail.com>
2024-09-21 00:12:49 -0500

    fix: complete testResults information
    - Add support for nested suites
    - Fix TestResultError(s) information
    - Fix TestSuiteResult(s) information
    - Fix TestResult(s) information
    - Update spec file: add new tests
    - Add Reporters to improve debugging

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -13,4 +13,23 @@ describe('a test suite', () => {
   it('should create element', () => {
     expect(element.innerHTML).toBe('hello there');
   });
+
+  describe('an inner test suite', () => {
+    it("should always be true: Level 2", () => {
+      expect(true).toBeTrue();
+      expect(undefined).toBeUndefined();
+      expect(null).toBeNull();
+      expect(["foo", "bar"]).toHaveSize(2);
+      expect({ "foo": "bar" }).toEqual({ "foo": "bar" });
+    });
+    describe('another inner test suite', () => {
+      it("should always be true: level 3", () => {
+        expect(true).toBeTrue();
+        expect(undefined).toBeUndefined();
+        expect(null).toBeNull();
+        expect(["foo", "bar"]).toHaveSize(2);
+        expect({ "foo": "bar" }).toEqual({ "foo": "bar" });
+      });
+    });
+  });
 });
